72337a021964600c43b3cc3b33e15c6cc304c4d4
王巨仙 <ayrikiya@wangjuxiandeMacBook-Pro.local>
2022-06-08 14:26:54 +0800

    ci: init commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: 王荣
+ * @Date: 2022-02-11 14:25:28
+ * @LastEditors: 王荣
+ * @LastEditTime: 2022-02-28 14:03:45
+ * @Description: 填写简介
+ */
+
+function App() {
+  return (
+    <div className="App">
+      <div>App</div>
+    </div>
+  );
+}
+
+export default App;
